ce59b946bd0d6dc6eb784b63e65bd998e8920027
Ryan Stevenson <ryanstev@usc.edu>
2018-11-09 16:59:39 -0800

    Init Commit

---
+++ MRTest2/Assets/Scripts/GazeComponent.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class GazeComponent : MonoBehaviour, IFocusable, IInputClickHandler
+{
+
+    [Tooltip("Audio clip to play when interacting with this hologram.")]
+    public AudioClip TargetFeedbackSound;
+    private AudioSource audioSource;
+
+    private GazeComponentManager manager;
+
+
+    private void EditManager(bool isActive)
+    {
+        if (manager == null)
+        {
+            manager = GameObject.FindObjectOfType<GazeComponentManager>();
+            if (manager == null)
+            {
+                return;
+            }
+        }
+        manager.SetComponentActive(this, isActive);
+        print("!!");
+    }
+    private void OnEnable()
+    {
+        EditManager(true);
+    }
+    private void OnDisable()
+    {
+        EditManager(false);
+    }
+    private void Start()
+    {
+        Collider collider = GetComponentInChildren<Collider>();
+        if (collider == null)
+        {
+            gameObject.AddComponent<BoxCollider>();
+        }
+
+        EnableAudioHapticFeedback();
+    }
+
+    private void EnableAudioHapticFeedback()
+    {
+        // If this hologram has an audio clip, add an AudioSource with this clip.
+        if (TargetFeedbackSound != null)
+        {
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+
+            audioSource.clip = TargetFeedbackSound;
+            audioSource.playOnAwake = false;
+            audioSource.spatialBlend = 1;
+            audioSource.dopplerLevel = 0;
+        }
+    }
+
+    void IFocusable.OnFocusEnter()
+    {
+        print("Gazing at"+gameObject.name);
+        manager.EnableGaze(this);
+    }
+
+    void IFocusable.OnFocusExit()
+    {
+        manager.DisableGaze(this);
+    }
+
+    void IInputClickHandler.OnInputClicked(InputClickedEventData eventData)
+    {
+        // Play the audioSource feedback when we gaze and select a hologram.
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+        
+    }
+
+    private void OnDestroy()
+    {
+    }
+}
---
+++ MRTest2/Assets/Scripts/GazeComponentManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeComponentManager : MonoBehaviour {
+
+    private Dictionary<GazeComponent,float> components;
+    private Dictionary<GazeComponent, Coroutine> coroutines;
+
+    private float totalTime = 0;
+
+    [SerializeField]
+    private GameObject cubePrefab;
+
+    [SerializeField]
+    private Transform ui;
+    // Use this for initialization
+    void Awake () {
+        components = new Dictionary<GazeComponent, float>();
+        coroutines = new Dictionary<GazeComponent, Coroutine>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.C))
+        {
+            Instantiate(cubePrefab, new Vector3(Random.Range(-3, 3), 0, 10),Quaternion.identity);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(PoolTest());
+        }
+	}
+    public void EnableGaze(GazeComponent target)
+    {
+        coroutines.Add(target, StartCoroutine(Gazing(target)));
+    }
+    private IEnumerator Gazing(GazeComponent target)
+    {
+        while (true)
+        {
+            totalTime += Time.deltaTime;
+            components[target] += Time.deltaTime;
+            ui.position = HighestGaze().transform.position + new Vector3(1, 0, 0);
+            PercentGazeAll();
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private IEnumerator PoolTest()
+    {
+        GameObject cube1 = transform.GetChild(0).gameObject;
+        cube1.SetActive(false);
+        yield return new WaitForSeconds(1);
+        cube1.SetActive(true);
+    }
+    public void DisableGaze(GazeComponent target)
+    {
+        StopCoroutine(coroutines[target]);
+        coroutines.Remove(target);
+    }
+    public float GetPercentGaze(GazeComponent target)
+    {
+        return (components[target]/totalTime);
+    }
+    public void PercentGazeAll()
+    {
+        string toprint = "";
+        foreach (var item in components)
+        {
+            toprint+= item.Key.gameObject.name+":"+GetPercentGaze(item.Key)+" ";
+        }
+        print(toprint);
+    }
+    public GazeComponent HighestGaze()
+    {
+        float highest = -1;
+        GazeComponent toreturn = null;
+        foreach (var item in components)
+        {
+            if (item.Value>highest)
+            {
+                highest = item.Value;
+                toreturn = item.Key;
+            }
+        }
+        return toreturn;
+    }
+
+    public void SetComponentActive(GazeComponent component,bool setactive=true)
+    {
+        if (setactive)
+        {
+            components.Add(component,0);
+        }
+        else if (components.ContainsKey(component))
+        {
+            totalTime -= components[component];
+            components.Remove(component);
+        }
+    }
+}
